4022c33c34eb1908e14a7b21bb6af43d0471afd1
doggo251 <37166660+doggo251@users.noreply.github.com>
2018-03-18 19:44:46 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+var shortid = require('shortid');
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+app.set('port', 2800);
+app.use('/static', express.static(__dirname + '/static'));
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+// Starts the server.
+server.listen(2800, function() {
+  console.log('Starting server on port 2800');
+});
+setInterval(function() {
+}, 1000);
+var players = {};
+var shots = {};
+io.on('connection', function(socket) {
+	console.log('server player id: ' + socket.id);
+  socket.on('new player', function(name) {
+    players[socket.id] = {
+      x: 400,
+      y: 300,
+      uName : name,
+      id : socket.id,
+      r : 18
+
+    };
+  });
+  socket.on('shot fired', function(xG,yG,sX,sY,owner){
+  	shotId = shortid.generate();
+  	var moveX = xG - sX;
+  	var moveY = yG - sY;
+  	if(moveX/moveY < 1 && moveX/moveY > -1){
+  		var sVelX = Math.abs((moveX/moveY)*5);
+  		var sVelY = 5;
+  	}
+  	else{
+  		var sVelY = Math.abs((moveY/moveX)*5);
+  		var sVelX = 5;
+  	}
+  	if(moveX < 1){
+  		sVelX = -sVelX;
+  	}
+  	if(moveY < 1){
+  		sVelY = -sVelY;
+  	}
+  	shots[shotId] = {
+  		id : shotId,
+  		shotX : sX,
+  		shotY : sY,
+  		xS : sVelX,
+  		yS : sVelY,
+  		cr : owner
+  	};
+  	console.log(shots[shotId].id);
+  });
+
+  socket.on('movement', function(data) {
+    var player = players[socket.id] || {};
+    if (data.left) {
+      player.x -= 5;
+    }
+    if (data.up) {
+      player.y -= 5;
+    }
+    if (data.right) {
+      player.x += 5;
+    }
+    if (data.down) {
+      player.y += 5;
+    }
+  });
+
+  socket.on('disconnect', function() {
+    	delete players[socket.id];
+  });
+});
+setInterval(function() {
+  io.sockets.emit('state', players);
+  io.sockets.emit('shotLoc', shots);
+  io.sockets.emit('sendPlayers', players);
+  for(var scan in shots){
+  	shots[scan].shotX += shots[scan].xS;
+  	shots[scan].shotY += shots[scan].yS;
+  	for(var id in players){
+  		if(shots[scan].shotX > (players[id].x - players[id].r) && shots[scan].shotX < (players[id].x + players[id].r) && shots[scan].shotY > (players[id].y - players[id].r) && shots[scan].shotY < (players[id].y + players[id].r) && players[id].id != shots[scan].cr){
+  			players[id].r -= 2;
+  			var hit = true;
+  			break;
+  		}
+  		else{
+  			var hit = false;
+  		}
+  	}
+  	if(shots[scan].shotX > 800 || shots[scan].shotY > 600 || shots[scan].shotX < 0 || shots[scan].shotY < 0 || hit){
+  		delete shots[scan];
+  }
+  }
+}, 1000 / 60);
---
+++ static/game.js
@@ -0,0 +1,129 @@
+var socket = io();
+var clientId;
+var insX;
+var insY;
+var xNowi;
+var yNowi;
+
+socket.on('message', function(data) {
+  console.log(data);
+});
+socket.on('name', function(data) {
+  // data is a parameter containing whatever data was sent
+});
+socket.on('connect', function(){
+	cId = socket.id;
+	console.log('client id: ' + cId);
+});
+var userName = prompt("enter a username");
+var movement = {
+  up: false,
+  down: false,
+  left: false,
+  right: false
+}
+document.addEventListener('keydown', function(event) {
+  switch (event.keyCode) {
+    case 65: // A
+      movement.left = true;
+      break;
+    case 87: // W
+      movement.up = true;
+      break;
+    case 68: // D
+      movement.right = true;
+      break;
+    case 83: // S
+      movement.down = true;
+      break;
+  }
+});
+document.addEventListener('keyup', function(event) {
+  switch (event.keyCode) {
+    case 65: // A
+      movement.left = false;
+      break;
+    case 87: // W
+      movement.up = false;
+      break;
+    case 68: // D
+      movement.right = false;
+      break;
+    case 83: // S
+      movement.down = false;
+      break;
+  }
+});
+//var mousex = event.clientX;
+//var mousey = event.clientY; 
+socket.emit('new player', userName);
+setInterval(function() {
+  socket.emit('movement', movement);
+}, 1000 / 60);
+//----------------viewport----------------------
+
+//----------------------------------------------
+var wW = window.innerWidth;
+var wH = window.innerHeight;
+var cW = wW*(3/4);
+var cH = wH*(3/4);
+var xCoe = cW/800;
+var yCoe = cH/600;
+var avgCoe = (xCoe + yCoe)/2;
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+ctx.canvas.width = 800*xCoe;
+ctx.canvas.height = 600*yCoe;
+socket.on('state', function(players) {
+  ctx.clearRect(0, 0, 800*xCoe, 600*yCoe);
+console.log('tick');
+  for (var id in players) {
+  	ctx.fillStyle = 'green';
+    var player = players[id];
+    var radius = player.r;
+    ctx.beginPath();
+    ctx.arc(player.x*xCoe, player.y*yCoe, radius*avgCoe, 0, 2 * Math.PI);
+    ctx.fill(); 
+    ctx.fillStyle = 'white';
+    ctx.font="10px Verdana";
+    ctx.fillText(player.uName,player.x*xCoe - 16,player.y*yCoe - 3);
+    ctx.fillText(radius,player.x*xCoe - 8,player.y*yCoe + 8);
+    ctx.closePath();
+    if(player.id == cId){
+    	xNowi = player.x;
+  		yNowi  = player.y;
+    }
+
+  }
+
+
+});
+socket.on('shotLoc', function(shots) {
+	for(var scan in shots){
+		ctx.beginPath();
+  	ctx.arc(shots[scan].shotX*xCoe, shots[scan].shotY*yCoe, 10*avgCoe, 0, 2 * Math.PI); 
+  	ctx.stroke();
+  	ctx.fill();
+  }
+  ctx.closePath();
+});
+socket.on('sendPlayers', function(players) {
+
+});
+document.addEventListener('mousedown', function(event) {
+	getCursorPosition(canvas,event);
+
+});
+function getCursorPosition(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+    insX = x/xCoe;
+    insY = y/yCoe;
+    console.log("x: " + insX + " y: " + insY);
+    socket.emit('shot fired', insX, insY, xNowi, yNowi, cId);
+}
+
+
+
+
